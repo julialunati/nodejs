c20f8cd03a76e3097d8a1e633c093056f7f42b69
julialunati <julia.molokova11@icloud.com>
2022-01-15 21:50:00 +0100

    DZ1

--- index.js
+++ index.js
@@ -1,8 +1,50 @@
-//  console.log(process.argv);
-//const [ name ] = process.argv.slice(2);
-const arg = process.argv.slice(2);
-console.log(arg);
-console.log(`hello, ${arg[1]}`);
+//dz1 
 
 const colors = require('colors');
-console.log(colors.america(`hello, ${arg[1]}`));
+
+const arg = process.argv.slice(2);
+
+function isPrime(i, n) {
+    let arr = [];
+    for (i; i <= n; i++) {
+        let flag = 1;
+        if (i > 2 && i % 2 != 0) {
+            for (let j = 3; j * j <= i; j = j + 2) {
+                if (i % j == 0) {
+                    flag = 0;
+                    break;
+                }
+            }
+        } else if (i != 2) flag = 0;
+        if (flag == 1) {
+            arr.push(i);
+        }
+    }
+    return arr;
+}
+
+if (Number.isInteger(+arg[0]) && Number.isInteger(+arg[1]) && arg[0] > 0 && arg[1] > 0 && arg[0] < arg[1]) {
+
+    let arr = isPrime(+arg[0], +arg[1]);
+
+    if (arr.length === 0) {
+        console.log(colors.red("There's no prime numbers!"));
+    } else {
+        for (let i = 0; i < arr.length; i += 3) {
+            if (i in arr) {
+                console.log(colors.green(arr[i]));
+            }
+            if (i + 1 in arr) {
+                console.log(colors.yellow(arr[i + 1]));
+            }
+            if (i + 2 in arr) {
+                console.log(colors.red(arr[i + 2]));
+            }
+        }
+    }
+
+} else {
+
+    console.log("Range error, please provide numbers with a positive value");
+
+}
